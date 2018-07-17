e791a32c342ad0074ba9dea0bca9137285d50f92
attilaKincsei <akincsei@gmail.com>
2018-07-17 16:07:38 +0200

    navbar implemented on main page. browse by product categories links are embedded in navbar

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -30,23 +30,20 @@ public class ProductController extends HttpServlet {
         List<Product> productList = productDataStore.getAll();
 
         String categoryParameter = req.getParameter("category");
-
-        Map params = new HashMap<>();
+        WebContext context = new WebContext(req, resp, req.getServletContext());
         if (categoryParameter != null) {
             int productCategoryId = Integer.parseInt(categoryParameter);
 
             if (productCategoryId > 0 && productCategoryId < 3) {
                 productList = productDataStore.getBy(productCategoryDataStore.find(productCategoryId));
-                params.put("category", productCategoryDataStore.find(productCategoryId));
             }
         }
 
-        params.put("products", productList);
-        params.put("recipient", "World");
+        context.setVariable("categories", productCategoryDataStore.getAll());
+        context.setVariable("products", productList);
+        context.setVariable("recipient", "World");
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
-        WebContext context = new WebContext(req, resp, req.getServletContext());
-        context.setVariables(params);
         engine.process("product/index.html", context, resp.getWriter());
     }
 

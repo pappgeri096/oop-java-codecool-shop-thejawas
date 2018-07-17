4592fe04ea708667b200e3439e1b2c8f6ee10278
Papp Gergely <pappgergo096@gmail.com>
2018-07-17 13:12:28 +0200

    new bootstrap theme

---
+++ src/main/java/com/codecool/shop/controller/PayedController.java
@@ -0,0 +1,45 @@
+package com.codecool.shop.controller;
+
+
+import com.paypal.api.payments.*;
+import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.PayPalRESTException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+@WebServlet(urlPatterns = {"/done"})
+public class PayedController extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String clientId = "AWTqmvOfxu2VnNifNQblRmD8ty6zvuam7Hh_k36MHk8sbYuZdEtR3gneLyuK_3A7E_AzZm0AWr-rNVA3";
+        String clientSecret = "ECYgXqdlLBxQsCHhdwMt4yz1LU5O5n6chmJe3EHrhGftsUOiN5PbmergN_0_lqQcFl-JzzC1ep68JG5I";
+
+        Payment payment = new Payment();
+        payment.setId(req.getParameter("paymentId"));
+
+        PaymentExecution paymentExecution = new PaymentExecution();
+        paymentExecution.setPayerId(req.getParameter("PayerID"));
+        try {
+            APIContext apiContext = new APIContext(clientId, clientSecret, "sandbox");
+            Payment createdPayment = payment.execute(apiContext, paymentExecution);
+            System.out.println(createdPayment.getPayer());
+
+        } catch (PayPalRESTException e) {
+            System.err.println(e.getDetails());
+        }
+
+    }
+
+}
--- src/main/java/com/codecool/shop/controller/PaymentController.java
+++ src/main/java/com/codecool/shop/controller/PaymentController.java
@@ -1,8 +1,70 @@
 package com.codecool.shop.controller;
 
+
+import com.paypal.api.payments.*;
+import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.PayPalRESTException;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 
 @WebServlet(urlPatterns = {"/payment"})
 public class PaymentController extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String clientId = "AWTqmvOfxu2VnNifNQblRmD8ty6zvuam7Hh_k36MHk8sbYuZdEtR3gneLyuK_3A7E_AzZm0AWr-rNVA3";
+        String clientSecret = "ECYgXqdlLBxQsCHhdwMt4yz1LU5O5n6chmJe3EHrhGftsUOiN5PbmergN_0_lqQcFl-JzzC1ep68JG5I";
+
+        Amount amount = new Amount();
+        amount.setCurrency("USD");
+        amount.setTotal("100");
+
+        Transaction transaction = new Transaction();
+        transaction.setAmount(amount);
+        List<Transaction> transactions = new ArrayList<Transaction>();
+        transactions.add(transaction);
+
+        Payer payer = new Payer();
+        payer.setPaymentMethod("paypal");
+
+        Payment payment = new Payment();
+        payment.setIntent("sale");
+        payment.setPayer(payer);
+        payment.setTransactions(transactions);
+
+        RedirectUrls redirectUrls = new RedirectUrls();
+        redirectUrls.setCancelUrl("https://example.com/cancel");
+        redirectUrls.setReturnUrl("http://localhost:8080/done");
+        payment.setRedirectUrls(redirectUrls);
+
+
+        try {
+            APIContext apiContext = new APIContext(clientId, clientSecret, "sandbox");
+            Payment createdPayment = payment.create(apiContext);
+
+            Iterator links = createdPayment.getLinks().iterator();
+            while (links.hasNext()) {
+                Links link = (Links) links.next();
+                if (link.getRel().equalsIgnoreCase("approval_url")) {
+                    // REDIRECT USER TO link.getHref()
+                    resp.sendRedirect(link.getHref());
+                }
+            }
+        } catch (PayPalRESTException e) {
+            System.err.println(e.getDetails());
+        }
+
+    }
+
 }

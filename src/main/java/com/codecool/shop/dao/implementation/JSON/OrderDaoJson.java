14459c28e904d4e28e274ea83558e533ee3f4c8e
attilaKincsei <akincsei@gmail.com>
2018-07-18 21:34:18 +0200

    git merge conflict resolved

---
+++ src/main/java/com/codecool/shop/dao/implementation/JSON/OrderDaoJson.java
@@ -0,0 +1,33 @@
+package com.codecool.shop.dao.implementation.JSON;
+
+import com.codecool.shop.dao.OrderDao;
+import com.codecool.shop.model.Order;
+
+import java.util.List;
+
+public class OrderDaoJson implements OrderDao {
+    @Override
+    public void add(Order order) {
+
+    }
+
+    @Override
+    public Order find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<Order> getAll() {
+        return null;
+    }
+
+    @Override
+    public Order getCurrent() {
+        return null;
+    }
+}

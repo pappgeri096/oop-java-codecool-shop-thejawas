619e392c5aa65b16a030e76e68dd519554b90a56
Papp Gergely <pappgergo096@gmail.com>
2018-07-18 12:09:12 +0200

    new bootstrap theme

---
+++ src/main/java/com/codecool/shop/controller/CreditCardValidator.java
@@ -0,0 +1,53 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+
+@WebServlet(urlPatterns = {"/validate"})
+public class CreditCardValidator extends HttpServlet {
+
+    private boolean validateCreditCard(int[] digits){
+        int sum = 0;
+        int length = digits.length;
+        for (int i = 0; i < length; i++) {
+
+            // get digits in reverse order
+            int digit = digits[length - i - 1];
+
+            // every 2nd number multiply with 2
+            if (i % 2 == 1) {
+                digit *= 2;
+            }
+            sum += digit > 9 ? digit - 9 : digit;
+        }
+        return sum % 10 == 0;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        engine.process("payment/index.html", context, resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String cardNumbers = "34534534543";
+        String[] numbers = cardNumbers.split("");
+       // int[] digits = Arrays.stream(numbers).map(Integer::parseInt);
+
+    }
+
+
+}

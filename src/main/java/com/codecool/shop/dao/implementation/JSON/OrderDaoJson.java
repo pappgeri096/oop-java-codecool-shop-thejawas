1f54cd587e6a0e329b3db0cc2a39f5779cc2085a
attilaKincsei <akincsei@gmail.com>
2018-07-19 16:39:32 +0200

    find(id), findAll() methods are implemented in OrderDaoJson

--- src/main/java/com/codecool/shop/dao/implementation/JSON/OrderDaoJson.java
+++ src/main/java/com/codecool/shop/dao/implementation/JSON/OrderDaoJson.java
@@ -6,18 +6,12 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.*;
 
 /**
- * TODO: DATA I NEED TO WRITE TO JSON FILE:
- * - Order ID: in Order.java
- * - Products name: in Order.java
- * - Number of products ordered: in Order.java
- * - User data: in Order.java
- * - Payment data: in Payment.java?????????
+ * OrderDaoJson class implements OrderDao class.
  * */
 public class OrderDaoJson implements OrderDao {
 
@@ -42,13 +36,74 @@ public class OrderDaoJson implements OrderDao {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-        System.out.println("ths method is executed");
+    /**
+     * finds file corresponding to one order based on UUID generated in the add() method
+     * */
+    @Override
+    public Order find(int id) {
+        return null;
+    }
 
+    public Map<String, String> find(String id) {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("target/orders/").append("Order_").append(id).append(".json");
+
+        Map<String, String> orderMap = new HashMap<>();
+        TypeReference<Map<String, String>> listTypeReference = new TypeReference<Map<String, String>>(){};
+        ObjectMapper orderReader = new ObjectMapper();
+        try {
+            orderMap = orderReader.readValue(new File(sb.toString()), listTypeReference);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return orderMap;
+    }
+
+    /**
+     * Reads orders from all files and adds them to a list of orders
+     * */
+    @Override
+    public List<Order> getAll() {
+        return null;
+    }
+
+    public List<Map<String, String>> getAllOrders() {
+        List<Map<String, String>> allOrderData = new ArrayList<>();
+        List<String> allOrderIds = getAllOrderIds();
+        for (String orderId : allOrderIds) {
+            allOrderData.add(find(orderId));
+        }
+        return allOrderData;
+    }
+
+    /**
+     * Creates UUID for order and order file name
+     * */
+    String createUuid() {
+        List<String> allOrderIds = getAllOrderIds();
+        System.out.println(allOrderIds);
+        boolean exists = false;
+        String newUuid;
+        do {
+            newUuid = UUID.randomUUID().toString();
+            for (String orderId :
+                    allOrderIds) {
+                if (orderId.equals(newUuid)) {
+                    exists = true;
+                    System.out.println("UUID already exists");
+                    break;
+                }
+            }
+        } while (exists);
+
+        return newUuid;
     }
 
     List<String> writeNewId(String newId) {
-        List<String> orderIdHistory = getAllOrderId();
+        List<String> orderIdHistory = getAllOrderIds();
         orderIdHistory.add(newId);
 
         ObjectMapper orderIdWriter = new ObjectMapper();
@@ -61,7 +116,7 @@ public class OrderDaoJson implements OrderDao {
         return orderIdHistory;
     }
 
-    List<String> getAllOrderId() {
+    List<String> getAllOrderIds() {
         List<String> orderIdHistory = new ArrayList<>();
         TypeReference<List<String>> listTypeReference = new TypeReference<List<String>>(){};
         ObjectMapper orderIdReader = new ObjectMapper();
@@ -97,10 +152,10 @@ public class OrderDaoJson implements OrderDao {
     }
 
     /**
-     * finds file corresponding to one order based on UUID generated in the add() method
+     * Gets current order from filelist? or ???
      * */
     @Override
-    public Order find(int id) {
+    public Order getCurrent() {
         return null;
     }
 
@@ -110,49 +165,5 @@ public class OrderDaoJson implements OrderDao {
     @Override
     public void remove(int id) {
 
-    }
-
-    /**
-     * Reads orders from all files and adds them to a list of orders
-     * */
-    @Override
-    public List<Order> getAll() {
-        return null;
-    }
-
-    /**
-     * Gets current order from filelist? or ???
-     * */
-    @Override
-    public Order getCurrent() {
-        return null;
-    }
-
-    /**
-     * Creates UUID for order and order file name
-     * */
-    String createUuid() {
-        List<String> allOrderIds = getAllOrderId();
-        System.out.println(allOrderIds);
-        boolean exists = false;
-        String newUuid;
-        do {
-            newUuid = UUID.randomUUID().toString();
-            for (String orderId :
-                    allOrderIds) {
-                if (orderId.equals(newUuid)) {
-                    exists = true;
-                    System.out.println("UUID already exists");
-                    break;
-                }
-            }
-        } while (exists);
-
-        return newUuid;
-    }
-
-    public static void main(String[] args) {
-        OrderDaoJson orderDao = new OrderDaoJson();
-        System.out.println("");
     }
 }
